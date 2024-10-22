08230ba1d85a74a090cec24a43583b129bc7f48b
Jhan Durango <jhandurango03@gmail.com>
2024-10-22 04:27:33 -0500

    add env variables

--- src/services/itemsServices.ts
+++ src/services/itemsServices.ts
@@ -1,26 +1,23 @@
 import axios from 'axios';
 
-const API_URL = 'https://optimal-tech-test-backend.onrender.com/items'; // Reemplaza con tu URL backend
+// Acceder a la URL de la API desde las variables de entorno
+const API_URL = `${import.meta.env.VITE_API_URL}/items`;
 
-// Obtener todos los items (GET)
 export const getItems = async () => {
   const response = await axios.get(API_URL);
   return response.data;
 };
 
-// Crear un nuevo item (POST)
 export const createItem = async (name: string) => {
   const response = await axios.post(API_URL, { name });
   return response.data;
 };
 
-// Actualizar un item existente (PUT)
 export const updateItem = async (id: number, name: string) => {
   const response = await axios.put(`${API_URL}/${id}`, { name });
   return response.data;
 };
 
-// Eliminar un item (DELETE)
 export const deleteItem = async (id: number) => {
   const response = await axios.delete(`${API_URL}/${id}`);
   return response.data;
